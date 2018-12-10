ccf7ea09f868bceeedfc8b037e7abfe0ab16a3b7
Amandine Latour <amandine.latour+github@gmail.com>
2018-12-10 16:40:21 +0900

    end of challenge 11

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,59 @@
+// Get our elements
+const video = () => document.querySelector(".player__video.viewer");
+const progressBar = () => document.querySelector(".progress__filled");
+const playerButton = () =>
+  document.querySelector(".player__button[title='Toggle Play']");
+const volumeSlider = () =>
+  document.querySelector('.player__slider[name="volume"]');
+const playBackSlider = () =>
+  document.querySelector('.player__slider[name="playbackRate"]');
+const tenSecondsButton = document.querySelector(
+  ".player__button[data-skip='-10']"
+);
+const twentyFiveSecondsButton = document.querySelector(
+  ".player__button[data-skip='25']"
+);
+
+// Build up our function
+const togglePlayPause = () => {
+  if (playerButton().classList.contains("toggle")) {
+    playerButton().classList.remove("toggle");
+    video().play();
+  } else {
+    playerButton().classList.add("toggle");
+    video().pause();
+  }
+};
+
+const changeVolume = e => {
+  video().volume = e.target.value;
+};
+
+const changePlayback = e => {
+  video().playbackRate = e.target.value;
+};
+
+const tenSecondsBack = e => {
+  video().currentTime -= 25;
+};
+
+const twentyFiveSecondsForward = e => {
+  video().currentTime += 25;
+};
+
+const updateProgressBar = e => {
+  progressBar().style.setProperty(
+    "flex-basis",
+    `${(e.target.currentTime * 100) / e.target.duration}%`
+  );
+};
+
+// Hook up the event listeners
+
+playerButton().addEventListener("click", togglePlayPause);
+video().addEventListener("click", togglePlayPause);
+video().addEventListener("timeupdate", updateProgressBar);
+volumeSlider().addEventListener("input", changeVolume);
+playBackSlider().addEventListener("input", changePlayback);
+tenSecondsButton.addEventListener("click", tenSecondsBack);
+twentyFiveSecondsButton.addEventListener("click", twentyFiveSecondsForward);
